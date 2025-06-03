c64d2c1d2fa8837a78a78140b77c5aeb466310d1
julmi <90mayanktyagi@gmail.com>
2025-06-03 18:09:33 +0530

    Recursion Ques 1 Pushed

--- julmi/Recursion/Ques_1_Recursion.py
+++ julmi/Recursion/Ques_1_Recursion.py
@@ -3,8 +3,6 @@
 '''
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
- 
-
 Example 1:
 Input: n = 3
 Output: ["((()))","(()())","(())()","()(())","()()()"]
@@ -17,3 +15,21 @@ Output: ["()"]
 ############################################### Solution In Python ##################################################
 
 #Solution 1: Brute Force 
+
+def backtrack(result, n, s='', left=0, right=0):
+    if len(s) == 2*n:
+        result.append(s)
+        return
+    if left < n:
+        backtrack(result, n, s+'(', left+1, right)
+    if right < left:
+        backtrack(result, n, s+')', left, right+1)
+        
+def generateParenthesis(n):
+    result = []
+    backtrack(result, n , '', 0, 0)
+    return result
+
+print(generateParenthesis(3))
+print(generateParenthesis(1))
+    
